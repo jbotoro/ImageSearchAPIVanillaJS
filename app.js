d39f3102b7d87a3eed054c17da3dc0247629c426
Jordan Black <jblack530@gmail.com>
2021-05-24 12:26:30 -0700

    added modal, still need to add media query css, and other styling

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const nextPage = document.getElementById("next-button")
 const prevPage = document.getElementById("prev-button")
 let currentPage = 1;
 let userQuery;
+let currentImages
 
 
 // handle search, send query to api endpoint
@@ -37,11 +38,47 @@ prevPage.addEventListener('click', () => {
 
 //event listener for modal open/close
 
-document.addEventListener('click', (e) => {
-    if (!e.target.matches('.result-list-wrapper')) return;
-    e.preventDefault();
-    console.log(e.target)
-}, false)
+// document.addEventListener('click', (e) => {
+//     if (!e.target.matches('.result-list-wrapper')) return;
+//     e.preventDefault();
+//     console.log(e.target)
+// }, false)
+
+// modal 
+
+const modalBackground = document.getElementById('modal-background');
+const modalContent = document.getElementById('modal-content');
+const closeButton = document.getElementsByClassName('close-modal')[0];
+const modalImage = document.getElementById('modal-image')
+
+const modalElement = element => document.querySelector(`.modal-background ${element}`);
+const body = document.querySelector('body');
+
+
+
+
+
+// openModal function
+
+function openModal(){
+    modalBackground.style.display = "block";
+}
+
+//close modal function and close modal on body click event listener
+
+function closeModal(){
+    modalBackground.style.display = 'none';
+}
+
+body.addEventListener("click", () => {
+    body.style.overflow = 'auto';
+    modalBackground.style.display = 'none';
+})
+
+
+
+
+
 
 //pagination function
 
@@ -78,6 +115,21 @@ async function fetchSearchResults(userQuery) {
         pagination(results.total_pages);
         // function  to display results  displayFunction(results);
         createResultsList(results);
+        currentImages = document.querySelectorAll(".result-list-wrapper img")
+        console.log(currentImages)
+        if(currentImages){
+            currentImages.forEach(img =>{
+                img.addEventListener('click', e => {
+                    console.log('image event listener fired')
+                    console.log(modalBackground)
+                    console.log(modalBackground.style.display)
+                    e.stopPropagation();
+                    modalBackground.style.display = 'block';
+                    modalImage.src = img.src; 
+                })
+            })
+            console.log(currentImages)
+        }
     } catch(err){
         console.log(err);
         alert('Search Failed');
@@ -89,24 +141,39 @@ async function fetchSearchResults(userQuery) {
 function createResultsList(resJSON) {
     const searchResults = document.querySelector(".search-results");
     searchResults.textContent= '';
-    console.log(searchResults);
+    // console.log(searchResults);
+    // console.log(resJSON.results)
     resJSON.results.forEach(res => {
         let url = res.urls.small;
-        let link = res.links.html;
+        // let id = res.id;
+        // let link = res.links.html;
         // let photographer = res.user.name;
         // let photographerLink = res.user.links.html;
         // let resLI = createListItem(res);
+        // searchResults.insertAdjacentHTML(
+        //     "beforeend",
+        //     `<div>
+        //         <div class="result-list-wrapper" id="result-list-wrapper">
+        //             <div 
+        //                 class="result-list-item" 
+        //                 style="background-image : url(${url});"
+        //             >
+
+        //             </div>
+        //         </div>
+        //     </div>`
+        // )
         searchResults.insertAdjacentHTML(
             "beforeend",
             `<div>
-                <a href=${url} target="_blank" class="result-list-wrapper">
-                    <div 
+                <div class="result-list-wrapper" id="result-list-wrapper">
+                    <img 
                         class="result-list-item" 
-                        style="background-image : url(${url});"
+                        src="${url}"
                     >
 
-                    </div>
-                </a>
+                    
+                </div>
             </div>`
         )
     })
